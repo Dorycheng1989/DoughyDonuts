0d75add31e97faa9ac0a606f05100e3fe00030d1
Doreen Achieng <dorychieng@gmail.com>
2023-05-10 06:48:34 +0300

    Update App.js
    removed checkout

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Home from './pages/Home';
 import AllFoods from './pages/AllFoods';
 import FoodDetails from './pages/FoodDetails';
-import Cart from './pages/Cart';
-import Checkout from './pages/Checkout';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Contact from './pages/Contact';
@@ -59,8 +57,6 @@ function App() {
       <Route path="/home" element={<Home />} />
       <Route path="/foods" element={<AllFoods />} />
       <Route path="/foods/:id" element={<FoodDetails />} />
-      <Route path="/cart" element={<Cart />} />
-      <Route path="/checkout" element={<Checkout />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/contact" element={<Contact />} />
